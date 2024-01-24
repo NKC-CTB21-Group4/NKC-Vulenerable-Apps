dc3b6ba1ed53010c21248b3a16894c2249f6bfbd
yuutakaki <nkc20216018@denpa.jp>
2024-01-24 13:31:12 +0900

    GETパラメータ受け取った値が正しいか確認するファイルno

--- challenges/public/levelPage.php
+++ challenges/public/levelPage.php
@@ -1,12 +1,15 @@
+<?php
+include ("switch.php");
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="LevelPage.css">
+  <link rel="stylesheet" href="levelPage.css">
 </head>
 <body>
-    <h1>脆弱性の名前</h1>
+    <h1><?php echo $title ?></h1>
     <div>
         <nav>
             <div>
