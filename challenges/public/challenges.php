a5e0061db6eb720d6909f519a9be70fcba28c448
take32457 <take32457@gmail.com>
2024-01-24 09:56:41 +0900

    first commit

---
+++ challenges/public/challenges.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>NKC Vulnerable Apps</title>
+</head>
+<body>
+  <div>
+    <nav id="container">                                                                                                                                                                                                                   
+        <div class="icon">
+            <img src="icon1.png" alt="Icon 1">
+            <p>インジェクション</p>
+        </div>
+        <div class="icon">
+            <img src="icon2.jpeg" alt="Icon 2">
+            <p>認証の不備</p>
+        </div>
+        <div class="icon">
+            <img src="icon3.png" alt="Icon 3">
+            <p>情報の露呈</p>
+        </div>
+        <div class="icon">
+            <img src="icon4.png" alt="Icon 4">
+            <p>XXE</p>
+        </div>
+        <div class="icon">
+            <img src="icon5.png" alt="Icon 5">
+            <p>ファイル削除の不備</p>
+        </div>
+        <div class="icon">
+            <img src="icon6.png" alt="Icon 6">
+            <p>XSS</p>
+        </div>
+        <div class="icon">
+            <img src="icon7.png" alt="Icon 7">
+            <p>ディレクトリライジング</p>
+        </div>
+        <div class="icon">
+            <img src="icon8.png" alt="Icon 8">
+            <p>コンポーネントミス設定</p>
+        </div>
+        <div class="icon">
+            <img src="icon9.png" alt="Icon 9">
+            <p>ロギングとモニタリング不足</p>
+        </div>
+    </nav>
+    <a href="/">戻る</a>
+  </div>
+</body>
+</html>
---
+++ challenges/public/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>NKC Vulnerable Apps</title>
+</head>
+<body>
+  <div>
+    <nav>
+      <div>
+        <a href="./challenges.php">練習問題</a>
+      </div>
+      <div>
+        <a href="http://localhost:3000">演習問題</a>
+      </div>
+      <div>
+        <a href="./progress.php">進捗状況</a>
+      </div>
+    </nav>
+  </div>
+</body>
+</html>
