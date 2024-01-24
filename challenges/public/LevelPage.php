7f6ad9979390bff1f002f255c2d215c906585c91
yuutakaki <nkc20216018@denpa.jp>
2024-01-24 12:02:56 +0900

    難易度選択画面の作成

---
+++ challenges/public/LevelPage.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="LevelPage.css">
+</head>
+<body>
+    <h1>脆弱性の名前</h1>
+    <div>
+        <nav>
+            <div>
+                <a href="">レベル１</a>
+            </div>
+            <div>
+                <a href="">レベル2</a>
+            </div>
+            <div>
+                <a href="">レベル3</a>
+            </div>
+            <div>
+                <a href="">レベル4</a>
+            </div>
+        </nav>
+    </div>
+</body>
+</html>
