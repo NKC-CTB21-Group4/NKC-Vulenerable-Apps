12f03b647eb07f6c12db4ee7feb8d3fc719acc20
hayashi0001 <hayasigame1@gmail.com>
2024-01-31 10:37:03 +0900

    challenges/public/images/
    画像の名前変更

--- challenges/public/challenges.php
+++ challenges/public/challenges.php
@@ -3,50 +3,74 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="challenges.css">
   <title>NKC Vulnerable Apps</title>
 </head>
 <body>
-  <div>
-    <nav id="container">                                                                                                                                                                                                                   
-        <div class="icon">
-            <img src="icon1.png" alt="Icon 1">
-            <p>インジェクション</p>
+    <div id="container">
+        <div class="box">
+            <div class="icon" onclick="window.location.href='level.php'">
+                <img src="../images/ダウンロード.png" alt="Icon 1">
+                <p>インジェクション</p>
+            </div>
         </div>
-        <div class="icon">
-            <img src="icon2.jpeg" alt="Icon 2">
-            <p>認証の不備</p>
+
+        <div class="box">
+            <div class="icon" onclick="window.location.href='level.php'">
+                <img src="../images/9930c6ec452b059eef859533dbdd7432_t.jpeg" alt="Icon 2">
+                <p>認証の不備</p>
+            </div>
         </div>
-        <div class="icon">
-            <img src="icon3.png" alt="Icon 3">
-            <p>情報の露呈</p>
+
+        <div class="box">
+            <div class="icon" onclick="window.location.href='level.php'">
+                <img src="../images/ダウンロード (1).png" alt="Icon 3">
+                <p>情報の露呈</p>
+            </div>
         </div>
-        <div class="icon">
-            <img src="icon4.png" alt="Icon 4">
-            <p>XXE</p>
+
+        <div class="box">
+            <div class="icon" onclick="window.location.href='level.php'">
+                <img src="../images/ダウンロード (1).png" alt="Icon 4">
+                <p>XXE</p>
+            </div>
         </div>
-        <div class="icon">
-            <img src="icon5.png" alt="Icon 5">
-            <p>ファイル削除の不備</p>
+
+        <div class="box">
+            <div class="icon" onclick="window.location.href='level.php'">
+                <img src="../images/ダウンロード (1).png" alt="Icon 5">
+                <p>ファイル削除の不備</p>
+            </div>
         </div>
-        <div class="icon">
-            <img src="icon6.png" alt="Icon 6">
-            <p>XSS</p>
+
+        <div class="box">
+            <div class="icon" onclick="window.location.href='level.php'">
+                <img src="../images/ダウンロード (1).png" alt="Icon 6">
+                <p>XSS</p>
+            </div>
         </div>
-        <div class="icon">
-            <img src="icon7.png" alt="Icon 7">
-            <p>ディレクトリライジング</p>
+
+        <div class="box">
+            <div class="icon" onclick="window.location.href='level.php'">
+                <img src="../images/ダウンロード (1).png" alt="Icon 7">
+                <p>ディレクトリライジング</p>
+            </div>
         </div>
-        <div class="icon">
-            <img src="icon8.png" alt="Icon 8">
-            <p>コンポーネントミス設定</p>
+
+        <div class="box">
+            <div class="icon" onclick="window.location.href='level.php'">
+                <img src="../images/ダウンロード (1).png" alt="Icon 8">
+                <p>コンポーネントミス設定</p>
+            </div>
         </div>
-        <div class="icon">
-            <img src="icon9.png" alt="Icon 9">
-            <p>ロギングとモニタリング不足</p>
+
+        <div class="box">
+            <div class="icon" onclick="window.location.href='level.php'">
+                <img src="../images/ダウンロード (1).png" alt="Icon 9">
+                <p>ロギングとモニタリング不足</p>
+            </div>
         </div>
-    </nav>
-    <a href="/">戻る</a>
-  </div>
+    </div>
+
 </body>
 </html>
