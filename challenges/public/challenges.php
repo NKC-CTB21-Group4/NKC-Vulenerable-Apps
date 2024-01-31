950ab3f3df79661b1f85578b764f7f8b5926e556
hayashi0001 <hayasigame1@gmail.com>
2024-01-31 10:50:41 +0900

    challenges/public/challenges.php
    challenges.phpのcommit

--- challenges/public/challenges.php
+++ challenges/public/challenges.php
@@ -10,63 +10,63 @@
     <div id="container">
         <div class="box">
             <div class="icon" onclick="window.location.href='level.php'">
-                <img src="../images/ダウンロード.png" alt="Icon 1">
+                <img src="../images/injection.png" alt="Icon 1">
                 <p>インジェクション</p>
             </div>
         </div>
 
         <div class="box">
             <div class="icon" onclick="window.location.href='level.php'">
-                <img src="../images/9930c6ec452b059eef859533dbdd7432_t.jpeg" alt="Icon 2">
+                <img src="../images/certification.jpeg" alt="Icon 2">
                 <p>認証の不備</p>
             </div>
         </div>
 
         <div class="box">
             <div class="icon" onclick="window.location.href='level.php'">
-                <img src="../images/ダウンロード (1).png" alt="Icon 3">
+                <img src="../images/Information.png" alt="Icon 3">
                 <p>情報の露呈</p>
             </div>
         </div>
 
         <div class="box">
             <div class="icon" onclick="window.location.href='level.php'">
-                <img src="../images/ダウンロード (1).png" alt="Icon 4">
+                <img src="../images/Information.png" alt="Icon 4">
                 <p>XXE</p>
             </div>
         </div>
 
         <div class="box">
             <div class="icon" onclick="window.location.href='level.php'">
-                <img src="../images/ダウンロード (1).png" alt="Icon 5">
+                <img src="../images/serialize.png" alt="Icon 5">
                 <p>ファイル削除の不備</p>
             </div>
         </div>
 
         <div class="box">
             <div class="icon" onclick="window.location.href='level.php'">
-                <img src="../images/ダウンロード (1).png" alt="Icon 6">
+                <img src="../images/xss.png" alt="Icon 6">
                 <p>XSS</p>
             </div>
         </div>
 
         <div class="box">
             <div class="icon" onclick="window.location.href='level.php'">
-                <img src="../images/ダウンロード (1).png" alt="Icon 7">
+                <img src="../images/Information.png" alt="Icon 7">
                 <p>ディレクトリライジング</p>
             </div>
         </div>
 
         <div class="box">
             <div class="icon" onclick="window.location.href='level.php'">
-                <img src="../images/ダウンロード (1).png" alt="Icon 8">
+                <img src="../images/component.png" alt="Icon 8">
                 <p>コンポーネントミス設定</p>
             </div>
         </div>
 
         <div class="box">
             <div class="icon" onclick="window.location.href='level.php'">
-                <img src="../images/ダウンロード (1).png" alt="Icon 9">
+                <img src="../images/log.png" alt="Icon 9">
                 <p>ロギングとモニタリング不足</p>
             </div>
         </div>
