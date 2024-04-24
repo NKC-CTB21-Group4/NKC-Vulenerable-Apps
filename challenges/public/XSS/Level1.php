0250ae0b7469d0f4710fd17f4aa24edd58d8a537
yuutakaki <nkc20216018@denpa.jp>
2024-04-24 15:00:37 +0900

    Level1の入力フォームの作成

--- challenges/public/XSS/Level1.php
+++ challenges/public/XSS/Level1.php
@@ -1,1 +1,16 @@
-Hello world
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" href="style.css" type="text/css">
+<head>
+    <title>基本的な入力フォーム</title>
+</head>
+<body>
+    <form action="/submit_form" method="post">
+        <label for="name">名前:</label><br>
+        <input type="text" id="name" name="name"><br>
+        <label for="email">メール:</label><br>
+        <input type="email" id="email" name="email"><br>
+        <input type="submit" value="送信">
+    </form>
+</body>
+</html>
