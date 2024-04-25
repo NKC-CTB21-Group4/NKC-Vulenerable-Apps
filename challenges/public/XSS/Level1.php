2d8b2e52bbaf3e66875a147cba8346f3bafff53f
yuutakaki <nkc20216018@denpa.jp>
2024-04-25 14:00:38 +0900

    XSSの脆弱性の実装

--- challenges/public/XSS/Level1.php
+++ challenges/public/XSS/Level1.php
@@ -5,12 +5,14 @@
     <title>基本的な入力フォーム</title>
 </head>
 <body>
-    <form action="/submit_form" method="post">
+    <form action="./Level1.php" method="GET">
         <label for="name">名前:</label><br>
         <input type="text" id="name" name="name"><br>
         <label for="email">メール:</label><br>
         <input type="email" id="email" name="email"><br>
         <input type="submit" value="送信">
     </form>
+    <h2><?php echo $_GET['name']?></h2>
+    <h2><?php echo $_GET['email']?></h2>
 </body>
 </html>
