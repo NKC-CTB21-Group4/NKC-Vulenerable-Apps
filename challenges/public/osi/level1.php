9383b6554b5c0fd7c80d9d91367f97315d79e54a
nn0525 <sibika3333@icloud.com>
2024-04-26 09:39:24 +0900

    最初の行のみ返すやつ

--- challenges/public/osi/level1.php
+++ challenges/public/osi/level1.php
@@ -2,22 +2,23 @@
 <html>
 <head>
     <title>Ping Test</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    <h2>Ping Test</h2>
-    <form method="post">
-        <label for="target">Enter hostname or IP address to ping:</label><br>
-        <input type="text" id="target" name="target"><br>
-        <input type="submit" value="Ping">
-    </form>
-    <br>
+    <div class="navindex">
+        <form method="post" class="divindex">
+            <label for="target">Enter hostname or IP address to ping:</label><br>
+            <input type="text" id="target" name="target"><br>
+            <input type="submit" value="Ping" class="aindex">
+        </form>
+    </div>
 
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $target = $_POST["target"];
         if (!empty($target)) {
-            $output = shell_exec("ping -c 4 " . $target); // 4回のping送信
-            echo "<pre>$output</pre>";
+            exec("ping -c 4 " . $target, $output); // 4回のping送信
+            echo '<div class="ping-result">' . $output[0] . '</div>'; // 最初の要素を表示
         } else {
             echo "Please enter a hostname or IP address to ping.";
         }
