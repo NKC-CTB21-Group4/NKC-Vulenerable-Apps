b3566858a3e2d0ddb64de3454beaf5deaf109ae7
yuutakaki <nkc20216018@denpa.jp>
2024-05-09 14:32:36 +0900

    passの修正

--- challenges/public/XSS/Level3.php
+++ challenges/public/XSS/Level3.php
@@ -1,5 +1,5 @@
 <?php
-require("../../utils/levelPageAccesCheck.php");
+require("../../utils/levelPageAccessBlock.php");
 if(AccessBlock())header("Location: /");
 // サニタイジング処理
 function sanitaizing($input){
