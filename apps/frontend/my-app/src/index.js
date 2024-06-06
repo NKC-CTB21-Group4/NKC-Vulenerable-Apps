628d7d50d32c4aae00105a5daaeef5a075dc7a00
yuutakaki <nkc20216018@denpa.jp>
2024-06-06 14:03:40 +0900

    contentファイルの作成

--- apps/frontend/my-app/src/index.js
+++ apps/frontend/my-app/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Content from './Component/Content';
+import DataRequest from './Utils/DataRequest';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <DataRequest/>
   </React.StrictMode>
 );
 
