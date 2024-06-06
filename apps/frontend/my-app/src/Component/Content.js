1f1135e05eeee922e2b32ff8bfc71b34b9553737
yuutakaki <nkc20216018@denpa.jp>
2024-06-06 14:51:51 +0900

    Content.jsファイルの修正

--- apps/frontend/my-app/src/Component/Content.js
+++ apps/frontend/my-app/src/Component/Content.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import './Content.css'; // CSS ファイルをインポート
 
-function Content({ contentData }) {
+function Content({ content }) {
   return (
-    <div className="content-wrapper">
-      {contentData.length > 0 ? (
-        contentData.map((item, index) => (
-          <div key={index} className="content-item">
-            <p className="content-text">{item.content}</p>
-          </div>
-        ))
-      ) : (
-        <div className="no-content">No content available</div>
-      )}
+    <div className="content-item">
+      <p className="content-text">{content}</p>
     </div>
   );
 }
