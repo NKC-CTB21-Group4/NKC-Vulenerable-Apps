d1f1a44b5ea4f560e129eeb4e3ff2c7a64d8b98a
take32457 <take32457@gmail.com>
2024-06-07 11:43:29 +0900

    必要なメソッドの追加

--- apps/backend/src/Domain/Main/Reaction/ReactionRepository.php
+++ apps/backend/src/Domain/Main/Reaction/ReactionRepository.php
@@ -6,5 +6,16 @@ namespace App\Domain\Main\Reaction;
 
 interface ReactionRepository
 {
-   
+   /**
+     * @param int $postId
+     * @return int
+     */
+    public function getFavsCountByPostId(int $id): int;
+
+    /**
+     * @param void
+     * @return bool
+     */
+    public function togglePostFav():bool;
+
 }
