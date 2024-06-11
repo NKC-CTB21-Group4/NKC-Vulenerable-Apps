af987fd962eb7c68cf935bec55c7bdae344e3d39
nn0525 <sibika3333@icloud.com>
2024-06-11 13:35:22 +0900

    文字の大きさ、配置調整完了

--- apps/frontend/my-app/src/Component/Userinfo.js
+++ apps/frontend/my-app/src/Component/Userinfo.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './Userinfo.css'; // CSSファイルをインポート
 
+
+
+
 function Userinfo({src,alt,username,userid }) {
+  
   return (
     <div className="userinfo-container">
     <img src={src} alt={alt} className="userinfo-icon" />
+    <div className="userinfo-text">
     <div className="userinfoname">{username}</div>
     <div className="userinfoid">{userid}</div>
+    </div>
+    
     </div>
   );
 }
