c96d458b7637b6724fa96428a1e953ad9c4daf8e
nn0525 <sibika3333@icloud.com>
2024-06-12 13:09:21 +0900

    userinfo.js css 完成

--- apps/frontend/my-app/src/Component/Userinfo.js
+++ apps/frontend/my-app/src/Component/Userinfo.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import './Userinfo.css'; // CSSファイルをインポート
+import Username from './Username'; // Usernameコンポーネントのインポート
+import Userid from './Userid'; // Useridコンポーネントのインポート
+import Icon from './Icon'; // Iconコンポーネントのインポート
 
 
 
-
-function Userinfo({src,alt,username,userid }) {
+function Userinfo({src, alt, username, userid}) {
   
+
   return (
     <div className="userinfo-container">
-    <img src={src} alt={alt} className="userinfo-icon" />
-    <div className="userinfo-text">
-    <div className="userinfoname">{username}</div>
-    <div className="userinfoid">{userid}</div>
-    </div>
-    
+      <Icon src={src} alt={alt} />
+      <div className="userinfo-text">
+        <Username username={username} />
+        <Userid userid={userid} />
+      </div>
     </div>
   );
 }
