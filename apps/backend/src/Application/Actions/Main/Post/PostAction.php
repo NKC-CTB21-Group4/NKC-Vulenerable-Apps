147fa3024f0f02d672924c4f1f31a342bcdc4db8
take32457 <take32457@gmail.com>
2024-06-13 13:44:49 +0900

    PostActionクラスの作成

--- apps/backend/src/Application/Actions/Main/Post/PostAction.php
+++ apps/backend/src/Application/Actions/Main/Post/PostAction.php
@@ -7,16 +7,20 @@ namespace App\Application\Actions\Main\Post;
 use App\Application\Actions\Action;
 use Psr\Log\LoggerInterface;
 use App\Domain\Main\Post\PostRepository;
+use App\Domain\Main\User\UserRepository;
 
 abstract class PostAction extends Action
 {
   protected PostRepository $postRepository;
+  protected UserRepository $userRepository;
 
   public function __construct(
     LoggerInterface $logger,
-    PostRepository $postRepository
+    PostRepository $postRepository,
+    UserRepository $userRepository
   ){
     parent::__construct($logger);
     $this->postRepository = $postRepository;
+    $this->userRepository = $userRepository;
   }
 }
