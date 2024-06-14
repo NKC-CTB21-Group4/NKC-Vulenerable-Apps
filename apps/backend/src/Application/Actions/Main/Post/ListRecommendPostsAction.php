fb343236885b6e139d4a7210c3d3faad91fb9c19
take32457 <take32457@gmail.com>
2024-06-14 12:14:53 +0900

    ListRecommendPostsActionクラスの作成

---
+++ apps/backend/src/Application/Actions/Main/Post/ListRecommendPostsAction.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Actions\Main\Post;
+
+use Psr\Http\Message\ResponseInterface as Response;
+
+use App\Domain\Main\Post\Post;
+
+class ListRecommendPostsAction extends PostAction
+{
+  protected function action(): Response
+  {
+    $posts = $this->postRepository->findAll();
+
+        $this->logger->info("Post List was viewed.");
+
+        return $this->respondWithData($posts);
+  }
+}
