56607536eba8cdcd6bedbe9214ec9651c5ee588d
nn0525 <sibika3333@icloud.com>
2024-06-14 11:17:45 +0900

    contentinfo用の修正

--- apps/frontend/my-app/src/Component/Fav.js
+++ apps/frontend/my-app/src/Component/Fav.js
@@ -8,7 +8,8 @@ function Fav({ postid }) {
         const fetchFavorites = async () => {
             try {
                 // Simulate fetching favorites from a server
-                const response = await fetch(`http://localhost:8080/api/favorite/posts/${postid}`);
+                //const response = await fetch(`http://localhost:8080/api/favorite/posts/${postid}`);
+                const response = await fetch("http://localhost:8081/favdata/fav.php");
                 const json = await response.json();
                 setFavorites(json.data);
             } catch (err) {
@@ -21,6 +22,7 @@ function Fav({ postid }) {
     const handleFavoriteClick = async () => {
         setError(null);
         try {
+            /*
             // Simulate an async API call to update favorites
             const response = await fetch(`http://localhost:8080/api/favorite/posts/${postid}`, {
                 method: 'POST',
@@ -28,7 +30,10 @@ function Fav({ postid }) {
                     'Content-Type': 'application/json',
                 },
             });
+            */
+            const response = await fetch("http://localhost:8081/favdata/fav.php");
             const json = await response.json();
+            console.log(json.data);
             setFavorites({fav:json.data.clicked ? favorites.fav + 1 : favorites.fav - 1, clicked:json.data.clicked});
         } catch (err) {
             setError('Failed to update favorites');
@@ -40,11 +45,12 @@ function Fav({ postid }) {
             <span onClick={handleFavoriteClick} 
                 style={{cursor: 'pointer', 
                         color: favorites.clicked ? 'red' : 'white',
+                        
                 }}
             >
-                ❤️
+                ♥
             </span>
-            <span>{favorites.fav}</span>
+            <span style={{color: 'black'}}>{favorites.fav}</span>
             {error && <span>{error}</span>}
         </div>
     );
