eb147eaa9685a6bdb28bcd96cd0ca3f3696265d3
take32457 <take32457@gmail.com>
2024-06-14 10:01:14 +0900

    routeの登録

--- apps/backend/app/routes.php
+++ apps/backend/app/routes.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use App\Application\Actions\Main\User\ViewUserAction;
 use App\Application\Actions\User\ListUsersAction;
 use App\Application\Actions\Main\Post\ViewPostAction;
+use App\Application\Actions\Main\Post\CreatePostAction;
 
 use App\Application\Middleware\Challenges\ChallengesJwtMiddleware;
 
@@ -55,6 +56,7 @@ return function (App $app) {
         $group->get('', ListUsersAction::class);
         $group->get('/{id}', ViewUserAction::class);
         $group->group('/{userId}/posts', function (Group $group) {
+            $group->post('',CreatePostAction::class);
             $group->get('/{postId}', ViewPostAction::class);
         });
     });
