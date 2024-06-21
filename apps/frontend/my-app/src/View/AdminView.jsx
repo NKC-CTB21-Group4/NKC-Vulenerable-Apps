ab337580a97c1bfa90e7cde7cf8928b25e1e3357
take32457 <take32457@gmail.com>
2024-06-21 12:11:47 +0900

    テストデータの削除

--- apps/frontend/my-app/src/View/AdminView.jsx
+++ apps/frontend/my-app/src/View/AdminView.jsx
@@ -6,18 +6,6 @@ import AdminHeader from '../Component/AdminHeader';
 import { useContext } from 'react';
 import AuthContext from '../Utils/AuthProvider';
 
-const posts = [
-  { id: 1, author_id: 100, author_name: 'John Doe', content: 'This is the first post.', createdAt: '2023-01-01' },
-  { id: 2, author_id: 101, author_name: 'Jane Smith', content: 'This is the second post.', createdAt: '2023-01-02' },
-  { id: 3, author_id: 102, author_name: 'Alice Johnson', content: 'This is the third post.', createdAt: '2023-01-03' },
-  { id: 4, author_id: 103, author_name: 'Bob Brown', content: 'This is the fourth post.', createdAt: '2023-01-04' },
-  { id: 5, author_id: 104, author_name: 'Charlie Davis', content: 'This is the fifth post.', createdAt: '2023-01-05' },
-  { id: 6, author_id: 105, author_name: 'David Evans', content: 'This is the sixth post.', createdAt: '2023-01-06' },
-  { id: 7, author_id: 106, author_name: 'Ella Fitzgerald', content: 'This is the seventh post.', createdAt: '2023-01-07' },
-  { id: 8, author_id: 107, author_name: 'Frank Harris', content: 'This is the eighth post.', createdAt: '2023-01-08' },
-  { id: 9, author_id: 108, author_name: 'Grace Kelly', content: 'This is the ninth post.', createdAt: '2023-01-09' },
-  { id: 10, author_id: 109, author_name: 'Henry Lee', content: 'This is the tenth post.', createdAt: '2023-01-10' },
-];
 
 
 export default function AdminView() {
@@ -33,7 +21,7 @@ export default function AdminView() {
       <AdminHeader />
       <Routes>
         <Route path="/users" element={<AdminUserList/>} />
-        <Route path="/posts" element={<AdminPostList rows={posts}/>} />
+        <Route path="/posts" element={<AdminPostList/>} />
         <Route path="/*" element={<Navigate to="/admin/users" />} />
       </Routes>
     </div>
