ff64c8663468669c3b28e449a5258c3080b09d93
yuutakaki <nkc20216018@denpa.jp>
2024-06-25 14:36:17 +0900

    ポスト削除機能の実装、他ファイルの修正

--- apps/frontend/my-app/src/Component/Contentinfo.js
+++ apps/frontend/my-app/src/Component/Contentinfo.js
@@ -3,20 +3,35 @@ import './Contentinfo.css'; // CSSファイルをインポート
 import Userinfo from './Userinfo';
 import Content from './Content';
 import Fav from './Fav';
+import deletePost from './DeletePost';
 
 
-function Contentinfo({src, alt, username, userid, postid, content}) {
-  
+function Contentinfo({src, alt, username, userid, postid, content,handleDelete}) {
+  const handleClick = async() =>{
+    try{
+      await deletePost(postid,userid)
+      handleDelete(postid)
+      alert('ポストが削除されました');
+    }
+    catch(error){
+      alert('ポストの削除に失敗しました');
+    }
+  }
 
   return (
     <div className="contentinfo-container">
       <div className="userinfo-content">
       <Userinfo src={src} alt={alt} username={username} userid={userid} />    
-    <div className="content">
-    <Content content={content}  />
-    </div>
-    </div>
+        <div className="content">
+        <Content content={content}  />
+        </div>
+      </div>
+      <div className='post-actions'>
       <Fav postid={postid}/>
+      <button className="post-delete-button" onClick={handleClick}>
+          削除
+      </button>
+      </div>
     </div>
   );
 }
