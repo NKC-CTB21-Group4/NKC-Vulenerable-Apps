3f7dee7b32964475348ea5a2b0140c1ecc46b080
nn0525 <sibika3333@icloud.com>
2024-06-25 11:23:25 +0900

    Mainviewにヘッダーを追加

--- apps/frontend/my-app/src/Component/Mainview.js
+++ apps/frontend/my-app/src/Component/Mainview.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './Mainview.css'; // CSSファイルをインポート
 import Linkview from './Linkview';
 import Postview from './Postview';
+import Header from './Header';
 
 function Mainview({ links, posts }) {
-  console.log("Links: ", links);
-  console.log("Posts: ", posts);
 
   return (
     <div className="mainview-container">
       <Linkview links={links} />
+    <div className="header-posts-container">
+      <Header/>
       <Postview posts={posts} /> {/* posts 全体を渡す */}
+    </div>
     </div>
   );
 }
