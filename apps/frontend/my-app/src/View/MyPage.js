28134aa976562450790f9a32ff6b2182fb6b822c
nn0525 <sibika3333@icloud.com>
2024-06-25 10:12:45 +0900

    Mypage完成

--- apps/frontend/my-app/src/View/MyPage.js
+++ apps/frontend/my-app/src/View/MyPage.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './MyPage.css'; // CSSファイルをインポート
 import Postview from '../Component/Postview';
+import Linkview from '../Component/Linkview';
 
 
-function Mypage({ posts = [], userId }) {
+function Mypage({ posts = [], userId, links }) {
   const myPosts = posts.filter(post => post.author_id === userId);
+  console.log(myPosts);
   return (
     <div className="mypage-container">
+      <div className="linkview-container">
+        <Linkview links={links} />
+      </div>
       <div className="postview-container">
         <Postview posts={myPosts} />
       </div>
