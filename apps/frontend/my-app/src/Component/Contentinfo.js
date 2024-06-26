5a69480524ca5a8d2694ec9a71dcc47a4afce285
yuutakaki <nkc20216018@denpa.jp>
2024-06-26 13:29:53 +0900

    PostViewファイルの作成

--- apps/frontend/my-app/src/Component/Contentinfo.js
+++ apps/frontend/my-app/src/Component/Contentinfo.js
@@ -1,36 +1,40 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Contentinfo.css'; // CSSファイルをインポート
 import Userinfo from './Userinfo';
 import Content from './Content';
 import Fav from './Fav';
 import deletePost from './DeletePost';
+import AuthContext from '../Utils/AuthProvider';
 
+function Contentinfo({ src, alt, username, userid, postid, content, handleDelete }) {
+  const { user } = useContext(AuthContext);
+  const currentUserId = user?.id;
 
-function Contentinfo({src, alt, username, userid, postid, content,handleDelete}) {
-  const handleClick = async() =>{
-    try{
-      await deletePost(postid,userid)
-      handleDelete(postid)
+  const handleClick = async () => {
+    try {
+      await deletePost(postid, userid);
+      handleDelete(postid);
       alert('ポストが削除されました');
-    }
-    catch(error){
+    } catch (error) {
       alert('ポストの削除に失敗しました');
     }
-  }
+  };
 
   return (
     <div className="contentinfo-container">
       <div className="userinfo-content">
-      <Userinfo src={src} alt={alt} username={username} userid={userid} />    
+        <Userinfo src={src} alt={alt} username={username} userid={userid} />
         <div className="content">
-        <Content content={content}  />
+          <Content content={content} />
         </div>
       </div>
-      <div className='post-actions'>
-      <Fav postid={postid}/>
-      <button className="post-delete-button" onClick={handleClick}>
-          削除
-      </button>
+      <div className="post-actions">
+        <Fav postid={postid} />
+        {currentUserId === userid && (
+          <button className="post-delete-button" onClick={handleClick}>
+            削除
+          </button>
+        )}
       </div>
     </div>
   );
