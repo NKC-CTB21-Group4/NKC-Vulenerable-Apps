7513c0af3de835e96d1f777221973b174fa827c6
take32457 <take32457@gmail.com>
2024-06-26 15:40:29 +0900

    Actionクラスの作成

---
+++ apps/backend/src/Application/Actions/Main/User/UploadUserAvatarAction.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Actions\Main\User;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\UploadedFileInterface;
+
+class UploadUserAvatarAction extends UserAction
+{
+
+    public function action(): Response
+    {
+        $directory = '/var/www/public/upload/avatar';
+        $user = $this->getUserFromToken();
+
+        $user = $this->checkUserAuthorization($user);
+        if ($user === null) {
+            return $this->respondWithData('Unauthorized', 403);
+        }
+
+        $userId = $user->getId();
+        $uploadedFiles = $this->request->getUploadedFiles();
+        $avatar = $uploadedFiles['avatar'] ?? null;
+
+        if (!($avatar instanceof UploadedFileInterface) || $avatar->getError() !== UPLOAD_ERR_OK) {
+          return $this->respondWithData("Failed to upload avatar", 400);
+        }
+
+        $filename = $this->moveUploadedFile($directory, $avatar, $userId);
+        $this->userRepository->setAvatarPath($user, $filename);
+        
+        return $this->respondWithData($filename, 200);
+    }
+
+    private function moveUploadedFile(string $directory, UploadedFileInterface $uploadedFile, int $userId)
+    {
+        $extension = pathinfo($uploadedFile->getClientFilename(), PATHINFO_EXTENSION);
+        $basename = sprintf('%s.%s', $userId, $extension);
+        $filename = $directory . DIRECTORY_SEPARATOR . $basename;
+        $uploadedFile->moveTo($filename);
+
+        return $filename;
+    }
+}
