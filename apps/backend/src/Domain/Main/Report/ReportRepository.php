7ee77bf703ce1721abdea977ed8dc1c999340b69
take32457 <take32457@gmail.com>
2024-07-05 13:08:41 +0900

    インタフェースの定義

---
+++ apps/backend/src/Domain/Main/Report/ReportRepository.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Main\Report;
+
+interface ReportRepository
+{
+   /**
+     * @param Report $report
+     * @return Report
+     * @throws ReportCreationFailedException;
+     */
+    public function create(Report $report): Report;
+
+    /**
+     * @return Report[]
+     */
+    public function findAll():array;
+
+    /**
+     * @param int $reportId
+     * @return Report
+     * @throws ReportNotFoundException
+     */
+    public function findByReportId(int $reportId):Report;
+
+    /**
+     * @param Post $post
+     * @return Report[]
+     */
+    public function findByPost(Post $post):array;
+
+    /**
+     * @return Report[]
+     */
+    public function getReportedPosts():array;
+
+    /**
+     * @param Tag $tag
+     * @return void 
+     */
+    public function addTag(Tag $tag):void;
+
+    /**
+     * @param Tag $tag
+     * @return void 
+     */
+
+     public function removeTag(Tag $tag):void;
+
+     /**
+      * @param int $tagId
+      * @return Report[]
+      */
+
+     public function findReportsWithTags(int $tagId): array;
+
+}
