49c1892114ec890c6dc6929a594465552c453b31
take32457 <take32457@gmail.com>
2024-07-05 13:09:22 +0900

    インタフェースの追加

---
+++ apps/backend/src/Domain/Main/Tag/TagRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Main\Tag;
+
+interface TagRepository
+{
+   /**
+     * @param Tag $tag
+     * @return void 
+     * @throws TagCreationFailedException;
+     */
+    public function create(Tag $tag): void;
+
+    /**
+     * @return Tag[]
+     */
+    public function findAll():array;
+
+    /**
+     * @param int $tagId
+     * @throws TagNotFoundException
+     * @return void
+     */
+    public function remove(int $id):void;
+
+}
