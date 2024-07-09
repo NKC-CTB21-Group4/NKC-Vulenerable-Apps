62c1b7c8cb02f42ed77dc8291dc1ba1cc0b191e3
take32457 <take32457@gmail.com>
2024-07-09 10:17:23 +0900

    Tagのインフラストラクチャクラスの作成

---
+++ apps/backend/src/Infrastructure/Persistence/Main/Tag/DatabaseTagRepository.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Persistence\Main\Tag;
+
+use App\Domain\Main\Tag\Tag;
+use App\Domain\Main\Tag\TagRepository;
+use App\Domain\Main\Tag\TagNotFoundException;
+use App\Domain\Main\Tag\TagCreationFailedException;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
+
+class DatabaseTagRepository extends EntityRepository implements TagRepository
+{
+    private EntityManager $entityManager;
+
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        parent::__construct($entityManager, $entityManager->getClassMetadata(Tag::class));
+    }
+
+    public function save(Tag $tag): void
+    {
+        $this->_em->persist($tag);
+        $this->_em->flush();
+    }
+
+    /**
+     * @param Tag $tag
+     * @throws TagCreationFailedException
+     */
+    public function create(Tag $tag): void
+    {
+        try {
+            $this->_em->persist($tag);
+            $this->_em->flush();
+        } catch (\Exception $e) {
+            throw new TagCreationFailedException('Failed to create tag.', 0, $e);
+        }
+    }
+
+    /**
+     * @return Tag[]
+     */
+    public function findAll(): array
+    {
+        return parent::findAll();
+    }
+
+    /**
+     * @param int $tagId
+     * @throws TagNotFoundException
+     * @return void
+     */
+    public function remove(int $tagId): void
+    {
+        $tag = parent::find($tagId);
+
+        if ($tag === null) {
+            throw new TagNotFoundException();
+        }
+
+        $this->_em->remove($tag);
+        $this->_em->flush();
+    }
+}
