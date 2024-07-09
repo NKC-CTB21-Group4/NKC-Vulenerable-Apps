f04133109c26fe3202f829c731b57c089c4a46b6
yuutakaki <nkc20216018@denpa.jp>
2024-07-09 13:56:03 +0900

    DirectMessage機能の作成

---
+++ apps/frontend/my-app/src/Component/DirectMessage.js
@@ -0,0 +1,76 @@
+import React, { useContext, useState, useEffect } from 'react';
+import UserList from './UserList';
+import MessageList from './MessageList.js';
+import MessageInput from './MessageInput';
+import './DirectMessage.css';
+import AuthContext from '../Utils/AuthProvider.jsx';
+
+function DirectMessage() {
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [messages, setMessages] = useState([]);
+  const [users, setUsers] = useState([]);
+  const {user} = useContext(AuthContext);
+  const userid = user?.id;
+
+  useEffect(() => {
+    // ユーザーリストを取得
+    if(!userid){
+        return;
+    }
+    fetch(`http://localhost:8080/users/${userid}/direct-message`,{
+        headers:{
+            "Authorization": "Bearer " + localStorage.getItem('authToken')
+        },
+    })
+      .then(response => response.json())
+      .then(json => setUsers(json.data))
+      .catch(error => console.error('Error fetching users:', error));
+  }, [userid]);
+
+  const handleUserSelect = (selectedUser) => {
+    setSelectedUser(selectedUser);
+    const senderid = selectedUser.sender_id;
+    const receiverid = selectedUser.receiver_id;
+    console.log(selectedUser);
+    fetch(`http://localhost:8080/direct-message/${userid}/${userid === receiverid ? senderid : receiverid}`,{
+        headers:{
+            "Authorization": "Bearer " + localStorage.getItem('authToken')
+        }
+    })
+      .then(response => response.json())
+      .then(json => setMessages(json.data))
+      .catch(error => console.error('Error fetching messages:', error));
+  };
+
+  const handleSendMessage = (message) => {
+    const receiverid = selectedUser.receiver_id;
+    const senderid = selectedUser.sender_id;
+    fetch(`http://localhost:8080/direct-message/${userid}/${userid === receiverid ? senderid : receiverid}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          "Authorization": "Bearer " + localStorage.getItem('authToken')
+        },
+        body: JSON.stringify({ message }),
+      })
+      .then(response => response.json())
+      .then(json => {
+        setMessages(prevMessages => [...prevMessages, { sender_id: userid, receiver_id: userid === receiverid ? senderid : receiverid, message, sent_at: new Date().toISOString() }]);
+      })
+      .catch(error => console.error('Error sending message:', error));
+  };
+
+  return (
+    <div className="direct-message-container">
+      <UserList users={users} onUserSelect={handleUserSelect} />
+      {selectedUser && (
+        <div className="chat">
+          <MessageList messages={messages} currentUserid={userid}/>
+          <MessageInput onSendMessage={handleSendMessage} />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default DirectMessage;
---
+++ apps/frontend/my-app/src/Component/MessageInput.js
@@ -0,0 +1,25 @@
+import React, {useState } from 'react';
+
+function MessageInput({ onSendMessage }) {
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSendMessage(message);
+    setMessage('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="message-input">
+      <input 
+        type="text" 
+        value={message} 
+        onChange={(e) => setMessage(e.target.value)} 
+        placeholder="メッセージを入力..."
+      />
+      <button type="submit" className='direct-message-button'>送信</button>
+    </form>
+  );
+}
+
+export default MessageInput;
