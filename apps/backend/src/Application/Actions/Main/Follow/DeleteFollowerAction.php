16954a09e930248be39bbe11ec1f80fc3cdc842c
hayashi0001 <hayasigame1@gmail.com>
2024-07-12 09:41:54 +0900

    apps/backend/src/Application/Actions/Main/Follow/DeleteFollowerAction.php
    フォロー削除機能

--- apps/backend/src/Application/Actions/Main/Follow/DeleteFollowerAction.php
+++ apps/backend/src/Application/Actions/Main/Follow/DeleteFollowerAction.php
@@ -2,24 +2,36 @@
 
 declare(strict_types=1);
 
-namespace App\Application\Action\Main\Follow;
+namespace App\Application\Actions\Main\Follow;
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use App\Domain\Main\User\User;
 use App\Domain\Main\Follow\Follow;
 use App\Domain\Main\Follow\FollowerNotFoundException;
 
 class DeleteFollowerAction extends FollowAction
 {
-    protected function action(Request $request): Response
+    protected function action(): Response
     {
-        $data = $request->getParsedBody();
+        // ユーザーのトークンからユーザー情報を取得
+        $user = $this->getUserFromToken();
 
-        $followerId = (int)$data['follower_id'];
-        $followedId = (int)$data['followed_id'];
+        // ユーザーの認可を確認
+        $follower = $this->checkUserAuthorization($user);
+        if ($follower === null) {
+            return $this->respondWithData('Unauthorized', 403);
+        }
 
         try {
-            $this->followRepository->deleteFollower($followerId, $followedId);
+            $followed = $this->userRepository->findUserOfId((int)$this->resolveArg('followedId'));
+        } catch (UserNotFoundException $e) {
+            $this->logger->info("User with id '{$this->resolveArg('followedId')}' not found.");
+            return $this->respondWithData("User Not Found.", 404);
+        }
+
+        try {
+            $this->followRepository->deleteFollower((int)$follower->getId(), (int)$followed->getId());
             return $this->respondWithData(['message' => 'Follower deleted successfully']);
         } catch (FollowerDeleteFailedException $e) {
             return $this->respondWithError('Failed to delete follower: ' . $e->getMessage(), 500);
