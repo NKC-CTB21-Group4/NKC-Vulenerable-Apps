e5cdfc3c73fccd324e6e849c64d726aa2169a76f
hayashi0001 <hayasigame1@gmail.com>
2024-07-19 10:19:51 +0900

    鍵あか切り替えaction作成

---
+++ apps/backend/src/Application/Actions/Main/User/PrivateUserAction.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Actions\Main\User;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use App\Domain\Main\User\User;
+use App\Domain\Main\User\UserRepository;
+use App\Domain\Main\User\UserPrivatedNotFoundException;
+
+class PrivateUserAction extends UserAction
+{
+    public function action() : Response{
+        $data = $this->getFormData();
+        $user = $this->getUserFromToken();
+
+        $user = $this->checkUserAuthorization($user);
+        if ($user === null) {
+            return $this->respondWithData('Unauthorized', 403);
+        }
+
+        $id = $user->getId();
+        if ($id === null) {
+            $this->logger->info("User toggle failed");
+            return $this->respondWithData('Invalid input', 400);
+        }
+
+        if (isset($data['isPrivate'])) {
+            $this->userRepository->privateToggleUser($user->getId(), (bool)$data['isPrivate']);
+        } else {
+            throw new UserPrivatedNotFoundException();
+        }
+
+        $this->logger->info("User toggle private successfully with ID: " . $user->getId());
+        return $this->respondWithData($user);
+
+    }
+}
