815528016ad1c7839b3c06222507a36b75ffffc3
yuutakaki <nkc20216018@denpa.jp>
2024-07-22 13:30:17 +0900

    ダイレクトメッセージ機能の修正

--- apps/frontend/my-app/src/Component/MessageList.js
+++ apps/frontend/my-app/src/Component/MessageList.js
@@ -17,9 +17,9 @@ function MessageList({ messages, currentUserid }) {
       {messages.map((message, index) => (
         <div
           key={index}
-          className={`direct-message ${message.sender_id === currentUserid ? 'sent' : 'received'}`}
+          className={`direct-message ${message.sender.id === currentUserid ? 'sent' : 'received'}`}
         >
-          <strong>{message.sender_id === currentUserid ? '' : `User ${message.sender_id}`}</strong> {message.message}
+          <strong>{message.sender.id === currentUserid ? '' : `${message.sender.username}`}</strong> {message.message}
           <em className="timestamp">{new Date(message.sent_at).toLocaleString()}</em>
         </div>
       ))}
