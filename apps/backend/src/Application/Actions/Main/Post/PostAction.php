20c3af2150c7750db3026fa114b8dda9db27bb08
hayashi0001 <hayasigame1@gmail.com>
2024-08-19 14:49:04 +0900

    findPublicPostIdで鍵垢判断機能追加

--- apps/backend/src/Application/Actions/Main/Post/PostAction.php
+++ apps/backend/src/Application/Actions/Main/Post/PostAction.php
@@ -9,37 +9,37 @@ use Psr\Log\LoggerInterface;
 use App\Domain\Main\Post\PostRepository;
 use App\Domain\Main\User\UserRepository;
 use App\Domain\Main\User\User;
+use App\Domain\Main\Follow\FollowRepository;
 use Psr\Http\Message\ResponseInterface as Response;
 
 abstract class PostAction extends Action
 {
-  protected PostRepository $postRepository;
-  protected UserRepository $userRepository;
+    protected PostRepository $postRepository;
+    protected UserRepository $userRepository;
+    protected FollowRepository $followRepository;
 
-  public function __construct(
-    LoggerInterface $logger,
-    PostRepository $postRepository,
-    UserRepository $userRepository
-  ){
-    parent::__construct($logger);
-    $this->postRepository = $postRepository;
-    $this->userRepository = $userRepository;
-  }
-
-  protected function getUserFromToken():?object
-  {
-      return (object)$this->request->getAttribute('token')['user'] ?? null;
-  }
-
-  protected function checkUserAuthorization(?object $user): ?User
-    {
-      if($user === null)return null;
-        $userId = (int) $this->resolveArg('userId');
-        $user = $this->userRepository->findUserOfId($user->id);
-        if ($user->getId() !== $userId) {
-            return null;
-        }
-        return $user;
+    public function __construct(
+        LoggerInterface $logger,
+        PostRepository $postRepository,
+        UserRepository $userRepository,
+        FollowRepository $followRepository
+    ){
+        parent::__construct($logger);
+        $this->postRepository = $postRepository;
+        $this->userRepository = $userRepository;
+        $this->followRepository = $followRepository;
     }
 
+    protected function getUserFromToken(): ?object
+    {
+        return (object)$this->request->getAttribute('token')['user'] ?? null;
+    }
+
+    protected function checkUserAuthorization(?object $user): ?User
+    {
+        if ($user === null) return null;
+        $userId = (int) $user->id;
+        $authenticatedUser = $this->userRepository->findUserOfId($userId);
+        return $authenticatedUser;
+    }
 }
