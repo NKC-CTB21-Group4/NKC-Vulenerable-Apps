d942308cbb34c4888069925198406603d815b709
yuutakaki <nkc20216018@denpa.jp>
2024-08-23 16:05:42 +0900

    user削除apiのファイルの作成

---
+++ apps/frontend/my-app/src/api/user.js
@@ -0,0 +1,49 @@
+const getAuthHeaders = () => {
+    const token = localStorage.getItem('authToken');
+    return token ? { 'Authorization': `Bearer ${token}` } : {};
+  };
+
+  export async function deleteUser(apiEndpoint) {
+    try {
+      const response = await fetch(apiEndpoint, {
+        method: 'DELETE',
+        headers: {
+          ...getAuthHeaders(),
+        },
+      });
+  
+      if (!response.ok) {
+        throw new Error('ユーザーの削除に失敗しました');
+      }
+      return response;
+    } catch (error) {
+      console.error('Error:', error);
+      throw error;
+    }
+  }
+
+  export async function getAuth(apiEndpoint,email,password) {
+    try {
+        const response = await fetch(apiEndpoint, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email,
+            password,
+          }),
+        });
+    
+        const responseData = await response.json();
+    
+        if (!response.ok) {
+          throw new Error(responseData.message || '認証に失敗しました');
+        }
+    
+        return responseData;
+      } catch (error) {
+        console.error('エラーが発生しました:', error.message);
+        throw error;
+      }
+  }
