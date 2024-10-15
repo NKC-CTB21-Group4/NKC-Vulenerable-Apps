8184ae956be1fda6abf6c1eaf43bcf503066157e
nn0525 <sibika3333@icloud.com>
2024-10-15 14:09:36 +0900

    修正

--- apps/frontend/my-app/src/Component/UserDelete.js
+++ apps/frontend/my-app/src/Component/UserDelete.js
@@ -90,7 +90,6 @@ const UserDelete = ({ userId }) => {
   return (
     <dialog id="user-delete-dialog">
       <h2>ユーザー削除</h2>
-      <button id="user-delete-modalBtn">キャンセル</button>
       <p>本当に削除しますか？</p>
       <form>
         <div className={authSuccess ? 'input-success' : ''}>
