04f8bcbfb0a6fa7652d8d47dd921e07bbc30d0a9
take32457 <take32457@gmail.com>
2024-10-18 09:49:47 +0900

    search機能の実装

--- apps/backend/app/routes.php
+++ apps/backend/app/routes.php
@@ -21,6 +21,7 @@ use App\Application\Actions\Main\Post\ListUserPostsAction;
 use App\Application\Actions\Main\Post\CreatePostAction;
 use App\Application\Actions\Main\Post\DeletePostAction;
 use App\Application\Actions\Main\Post\GetPostImageAction;
+use App\Application\Actions\Main\Post\SearchPostsAction;
 use App\Application\Actions\Main\Post\ListRecommendPostsAction;
 use App\Application\Middleware\Challenges\ChallengesJwtMiddleware;
 use App\Application\Middleware\Main\JwtMiddleware;
@@ -93,6 +94,7 @@ return function (App $app) {
     });
     
     $app->get('/posts',ListRecommendPostsAction::class);
+    $app->get('/posts/search',SearchPostsAction::class);
     $app->get('/posts/{imageId}',GetPostImageAction::class);
 
     $app->group('/favorite/posts',function(Group $group){
