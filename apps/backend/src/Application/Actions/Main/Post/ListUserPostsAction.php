cb2060bc5bd447db2b863d9f0fd3a490e95a17f2
hayashi0001 <hayasigame1@gmail.com>
2024-10-24 14:08:24 +0900

    鍵垢機能追加

--- apps/backend/src/Application/Actions/Main/Post/ListUserPostsAction.php
+++ apps/backend/src/Application/Actions/Main/Post/ListUserPostsAction.php
@@ -15,16 +15,40 @@ class ListUserPostsAction extends PostAction
 {
   protected function action():Response
   {
+    
     $userId = (int)$this->resolveArg('userId');
-    
-    try {
-      $user = $this->userRepository->findUserOfId($userId);
-    } catch (UserNotFoundException $e) {
-      $this->logger->info("user with id `$userId` not found.");
-      return $this->respondWithData("User Not Found.", 404);
+
+    // tokenからuserId特定->FollowerId特定
+    $userFromToken = $this->getUserFromToken();
+    $authenticatedUser = $this->checkUserAuthorization($userFromToken);
+    if ($authenticatedUser === null) {
+        return $this->respondWithData('Unauthorized', 403);
     }
+
+    $followerId = $authenticatedUser->getId();
+
+    // userIdからそのuserが鍵あかか判断
+    $user = $this->userRepository->findUserOfId($userId);
+    $isPrivate = $user->getIsPrivate();
+
+    $bothFollowCheck = $this->followRepository->bothFollowChecker($followerId, $userId);
+    if ($isPrivate && !$bothFollowCheck) {
+        return $this->respondWithData("This user is Private and you are not mutual followers.", 403);
+    }
+
+    try {
+      // 例外を返す可能性がある
+      $posts = $this->postRepository->findPostOfUser($user);
+  } catch (PostNotFoundException $e) {
+      $this->logger->info("Post with id `$postId` not found.");
+      return $this->respondWithData("Post Not Found.", 404);
+  }
+
+  if ($posts == null) {
+      $this->logger->info("This userId '$userId' is Private.");
+      return $this->respondWithData("This user is Private.", 405);
+  }
     
-    $posts = $this->postRepository->findPostOfUser($user);
     $username = $user->getUsername();
 
     $this->logger->info("${username}User's Post list was viewed.");
