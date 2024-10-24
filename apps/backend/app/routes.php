37ac2e410845d118898a25e7e35c580368c98d69
hayashi0001 <hayasigame1@gmail.com>
2024-10-24 14:09:25 +0900

    listuserPostActionにjwt追加

--- apps/backend/app/routes.php
+++ apps/backend/app/routes.php
@@ -115,7 +115,7 @@ return function (App $app) {
         $group->post('/{userId}/follow/{followedId}',AddFollowerAction::class)->add(JwtMiddleware::class);
         $group->delete('/{userId}/follow/{followedId}',DeleteFollowerAction::class)->add(JwtMiddleware::class);
         $group->group('/{userId}/posts', function (Group $group) {
-            $group->get('',ListUserPostsAction::class);
+            $group->get('',ListUserPostsAction::class)->add(JwtMiddleware::class);
             $group->post('',CreatePostAction::class)->add(JwtMiddleware::class);
             $group->get('/{postId}', ViewPublicPostAction::class)->add(JwtMiddleware::class);
             $group->delete('/{postId}',DeletePostAction::class)->add(JwtMiddleware::class);
