83a5b562e8e99efb506674ab90bd0e7c0ac7399e
hayashi0001 <hayasigame1@gmail.com>
2024-10-29 11:25:48 +0900

    getuserFormTokenの追加

--- apps/backend/src/Application/Actions/Main/Post/PostAction.php
+++ apps/backend/src/Application/Actions/Main/Post/PostAction.php
@@ -10,6 +10,7 @@ use App\Domain\Main\Post\PostRepository;
 use App\Domain\Main\User\UserRepository;
 use App\Domain\Main\User\User;
 use App\Domain\Main\Follow\FollowRepository;
+use App\Infrastructure\Persistence\Main\Auth\JwtService;
 use Psr\Http\Message\ResponseInterface as Response;
 
 abstract class PostAction extends Action
@@ -17,17 +18,20 @@ abstract class PostAction extends Action
     protected PostRepository $postRepository;
     protected UserRepository $userRepository;
     protected FollowRepository $followRepository;
+    protected JwtService $jwtService;
 
     public function __construct(
         LoggerInterface $logger,
         PostRepository $postRepository,
         UserRepository $userRepository,
-        FollowRepository $followRepository
+        FollowRepository $followRepository,
+        JwtService $jwtService
     ){
         parent::__construct($logger);
         $this->postRepository = $postRepository;
         $this->userRepository = $userRepository;
         $this->followRepository = $followRepository;
+        $this->jwtService = $jwtService;
     }
 
     protected function getUserFromToken(): ?object
@@ -41,5 +45,41 @@ abstract class PostAction extends Action
         $userId = (int) $user->id;
         $authenticatedUser = $this->userRepository->findUserOfId($userId);
         return $authenticatedUser;
+    }
+
+    protected function getUserFromHeader(): ?User
+    {
+        // Authorization ヘッダーを取得
+        $authHeader = $this->request->getHeader('Authorization');
+
+        // ヘッダーが存在しない、または空の場合は null を返す
+        if (empty($authHeader[0])) {
+            return null;
+        }
+
+        // ヘッダーが Bearer トークン形式か確認
+        if (!preg_match('/Bearer\s(\S+)/', $authHeader[0], $matches)) {
+            return null;
+        }
+
+        $token = $matches[1];
+
+        if (!$token) {
+            return null;
+        }
+
+        // トークンを検証
+        try {
+            $decoded = $this->jwtService->validateToken($token);
+            if (!isset($decoded["user"])) {
+                return null; // user が存在しない場合
+            }
+            $userData = $decoded["user"]; // stdClass の場合
+            $userId = $userData->id; // stdClass の場合
+            return $this->userRepository->findUserOfId((int)$userId);
+        } catch (\Exception $e) {
+            // トークンの検証に失敗した場合、null を返す
+            return null;
+        }
     }
 }
