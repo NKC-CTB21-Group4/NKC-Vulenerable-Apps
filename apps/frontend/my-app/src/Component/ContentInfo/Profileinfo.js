9a526ca456fa8e9f650701f2464d2ad52ae9a322
nn0525 <sibika3333@icloud.com>
2024-10-29 14:00:48 +0900

    不要なdivを削除

--- apps/frontend/my-app/src/Component/ContentInfo/Profileinfo.js
+++ apps/frontend/my-app/src/Component/ContentInfo/Profileinfo.js
@@ -10,12 +10,8 @@ function Profileinfo({src, alt, username, userid, profile}) {
 
   return (
     <div className="Profileinfo-container">
-        <div className="userinfo-container">
-            <Userinfo src={src} alt={alt} userid={userid} username={username}/>
-        </div>
-        <div className="profileinfo-profilecontainer">
-            <Profile profile={profile}/>
-        </div>
+        <Userinfo src={src} alt={alt} userid={userid} username={username}/>
+        <Profile profile={profile}/>
     </div>
   );
 }
