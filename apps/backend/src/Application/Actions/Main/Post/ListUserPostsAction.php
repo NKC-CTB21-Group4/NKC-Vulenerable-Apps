61e3294df6d757168746596759c7d1f2619994f7
hayashi0001 <hayasigame1@gmail.com>
2024-11-05 10:12:35 +0900

    ログインの有無、鍵垢判断、相互フォロー判断機能追加

--- apps/backend/src/Application/Actions/Main/Post/ListUserPostsAction.php
+++ apps/backend/src/Application/Actions/Main/Post/ListUserPostsAction.php
@@ -15,45 +15,66 @@ class ListUserPostsAction extends PostAction
 {
   protected function action():Response
   {
+    $userFromToken = $this->getUserFromHeader();
+    //自分
+    $userId = $userFromToken ? $userFromToken->getId() : null;
+    //相手
+    $followerId = (int)$this->resolveArg('userId');
+    $follower = $this->userRepository->findUserOfId($followerId);
+    $isPrivate = $follower->getIsPrivate();
     
-    $userId = (int)$this->resolveArg('userId');
+    if ($userId === null) {
+      //操作ユーザーがログインをしていなかったら
+      //Privateか判定
+      if($isPrivate === false) {
+        try {
+          //公開ユーザーの場合だけ表示
+          $posts = $this->postRepository->findPostOfUser($follower);
+        } catch (PostNotFoundException $e) {
+            $this->logger->info("Post with id `$posts` not found.");
+            return $this->respondWithData("Post Not Found.", 404);
+        }
+      } else {
+        return $this->respondWithData("This follower is private.", 404);
+      }
+    } else {
+        //操作ユーザーがログインをしていたら
+      $bothFollowCheck = $this->followRepository->bothFollowChecker($followerId, $userId);
+      if ($isPrivate && !$bothFollowCheck) {
+          return $this->respondWithData("This user is Private and you are not mutual followers.", 403);
+      }
+      $posts = $this->postRepository->findPostOfUser($follower);
+    }
 
-    // tokenからuserId特定->FollowerId特定
-    // $userFromToken = $this->getUserFromToken();
-    // $authenticatedUser = $this->checkUserAuthorization($userFromToken);
-    // if ($authenticatedUser === null) {
-    //     return $this->respondWithData('Unauthorized', 403);
-    // }
+  return $this->respondWithData($posts);
 
-    //$followerId = $this->userRepository->getId();
+  //   // userIdからそのuserが鍵あかか判断
+  //   $user = $this->userRepository->findUserOfId($userId);
+  //   $isPrivate = $user->getIsPrivate();
 
-    // userIdからそのuserが鍵あかか判断
-    $user = $this->userRepository->findUserOfId($userId);
-    // $isPrivate = $user->getIsPrivate();
+  //   $bothFollowCheck = $this->followRepository->bothFollowChecker($followerId, $userId);
+  //   if ($isPrivate && !$bothFollowCheck) {
+  //       return $this->respondWithData("This user is Private and you are not mutual followers.", 403);
+  //   }
 
-    // $bothFollowCheck = $this->followRepository->bothFollowChecker($followerId, $userId);
-    // if ($isPrivate && !$bothFollowCheck) {
-    //     return $this->respondWithData("This user is Private and you are not mutual followers.", 403);
-    // }
+  //   try {
+  //     // 例外を返す可能性がある
+  //     $posts = $this->postRepository->findPostOfUser($user);
+  //   } catch (PostNotFoundException $e) {
+  //       $this->logger->info("Post with id `$postId` not found.");
+  //       return $this->respondWithData("Post Not Found.", 404);
+  // }
 
-    try {
-      // 例外を返す可能性がある
-      $posts = $this->postRepository->findPostOfUser($user);
-    } catch (PostNotFoundException $e) {
-        $this->logger->info("Post with id `$postId` not found.");
-        return $this->respondWithData("Post Not Found.", 404);
-  }
-
-  if ($posts == null) {
-      $this->logger->info("This userId '$userId' is Private.");
-      return $this->respondWithData("This user is Private.", 405);
-  }
+  // if ($posts == null) {
+  //     $this->logger->info("This userId '$userId' is Private.");
+  //     return $this->respondWithData("This user is Private.", 405);
+  // }
     
-    $username = $user->getUsername();
+  //   $username = $user->getUsername();
 
-    $this->logger->info("${username}User's Post list was viewed.");
+  //   $this->logger->info("${username}User's Post list was viewed.");
 
-    return $this->respondWithData($posts);
+  //   return $this->respondWithData($posts);
+  // }
   }
-
 }
