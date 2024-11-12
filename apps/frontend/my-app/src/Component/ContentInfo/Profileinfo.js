62e75d9c5179aaa24d9e78dc1199e6a8278e0551
nn0525 <sibika3333@icloud.com>
2024-11-12 13:48:35 +0900

    マイページのアイコンクリックイベント追加

--- apps/frontend/my-app/src/Component/ContentInfo/Profileinfo.js
+++ apps/frontend/my-app/src/Component/ContentInfo/Profileinfo.js
@@ -5,15 +5,16 @@ import Profile from './Profile';
 
 
 
-function Profileinfo({src, alt, username, userid, profile}) {
+function Profileinfo({src, alt, username, userid, profile,onUserIconClick}) {
   
 
   return (
     <div className="Profileinfo-container">
-        <Userinfo src={src} alt={alt} userid={userid} username={username}/>
+        <Userinfo src={src} alt={alt} userid={userid} username={username} onUserIconClick={onUserIconClick}/>
         <Profile profile={profile}/>
     </div>
   );
 }
 
 export default Profileinfo;
+
