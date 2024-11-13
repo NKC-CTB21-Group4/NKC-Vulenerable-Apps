c204bbd233d504354be85d825f5ecbdaf49138d6
yuutakaki <nkc20216018@denpa.jp>
2024-11-13 14:47:22 +0900

    usernameに半角英数字以外を使用できないようにした

--- apps/frontend/my-app/src/View/CreateUser.js
+++ apps/frontend/my-app/src/View/CreateUser.js
@@ -63,11 +63,16 @@ const CreateUser = () => {
         <div className="create-user-form-group">
           <label className='create-user-label'>ユーザーネーム:</label>
           <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className="create-user-input"
-            required
+             type="text"
+             value={username}
+             onChange={(e) => {
+               // 半角英数字のみを許可
+               const newValue = e.target.value.replace(/[^a-zA-Z0-9_]/g, '');
+               setUsername(newValue);
+             }}
+             className="create-user-input"
+             required
+             maxLength={14} // 14文字の制限を追加
           />
         </div>
         <div className="create-user-form-group">
