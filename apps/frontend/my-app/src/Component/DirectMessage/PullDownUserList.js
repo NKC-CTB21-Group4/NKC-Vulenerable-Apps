3d2e5e4d7c15b16b17bd1d9291c53edce075b656
yuutakaki <nkc20216018@denpa.jp>
2024-11-13 13:49:19 +0900

    画像パスが存在しないときにデフォルトの画像を表示する修正

--- apps/frontend/my-app/src/Component/DirectMessage/PullDownUserList.js
+++ apps/frontend/my-app/src/Component/DirectMessage/PullDownUserList.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './css/PullDownUserList.css';
+import defaultAvatar from '../../images/tegakicreateuser.png'
 
 const PullDownUserList = ({ users, onUserClick }) => {
   return (
     <ul className="pull-down-user-list">
       {users.map(user => (
         <li key={user.id} onClick={() => onUserClick(user)}>
-        <img src={`http://localhost:8080${user.avatar_path}`} alt={`${user.username}'s avatar`} />
+        <img src={user.avatar_path ? `http://localhost:8080${user.avatar_path}` : defaultAvatar } alt={`${user.username}'s avatar`} />
         {user.username}
         </li>
       ))}
